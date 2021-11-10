cb29c768e7d6836359a199be663959dab2e644a1
LuckyWind_sck <luckywindsck@gmail.com>
2021-11-10 21:11:25 +0900

    WIP: modifying course object
    TODO: extract columns

--- src/buildCourseObject.ts
+++ src/buildCourseObject.ts
@@ -1,8 +1,10 @@
 import type { CheerioAPI } from 'cheerio';
 import cheerio from 'cheerio';
-import type { Course } from 'coursum-types';
+import type { CourseV2 } from 'coursum-types';
 
 import type { CoursePage } from './types';
+
+console.clear();
 
 const getDD = (label: string, dom: CheerioAPI) => (
   dom('dt')
@@ -19,64 +21,61 @@ const getLecturers = (dom: CheerioAPI, domEn: CheerioAPI) => {
 
   // TODO: check if namesJa.length === namesEn.length
   const lecturers = namesJa.map((_, i) => ({
-    id: null,
-    imgUrl: null,
     name: {
       ja: namesJa[i],
       en: namesEn[i],
     },
-    email: '',
-    inCharge: true,
   }));
 
   return lecturers;
 };
 
 const getSchedule = (dom: CheerioAPI, domEn: CheerioAPI) => {
-  const scheduleYear = parseInt(getDD('開講年度・学期', dom), 10);
+  const year = parseInt(getDD('開講年度・学期', dom), 10);
+
+  const semesterJa = getDD('開講年度・学期', dom).split(' ')[1];
+
+  const semester = {
+    ja: semesterJa,
+    en: semesterJa === '春学期' ? 'Spring' : 'Fall',
+  };
+
+  const dayRegJa = /[月火水木金土日]/;
+  const dayRegEn = /Mon|Tue|Wed|Thu|Fri|Sat|Sun/;
+
+  const getDayPeriod = (dayPeriodStr: string, dayReg: RegExp) => (
+    dayPeriodStr.split(',')
+      .map((time) => time.trim())
+      .map((time) => [time.match(dayReg)?.[0] ?? '', time.match(/\d/)?.[0] ?? ''])
+      .map(([day, period]) => day + period)
+  );
+
+  const times = {
+    ja: getDayPeriod(getDD('曜日・時限', dom), dayRegJa),
+    en: getDayPeriod(getDD('Day of Week・Period', domEn), dayRegEn),
+  };
+
+  // TODO: type
+  const span: any = {
+    ja: null,
+    en: null,
+  };
+
   const titleJa = dom('h2 .title').text();
 
-  const scheduleSemesterJa = getDD('開講年度・学期', dom).split(' ')[1];
-  const scheduleSemesterEn = scheduleSemesterJa === '春学期' ? 'Spring' : 'Fall';
-
-  const scheduleTimesJa = (
-    getDD('曜日・時限', dom).split(',')
-      .map((time) => time.trim())
-      .map((time) => [time.match(/[月火水木金土日]/)?.[0] ?? '', time.match(/\d/)?.[0] ?? ''])
-      .map(([day, period]) => day + period)
-  );
-  const scheduleTimesEn = (
-    getDD('Day of Week・Period', domEn).split(',')
-      .map((time) => time.trim())
-      .map((time) => [time.match(/Mon|Tue|Wed|Thu|Fri|Sat|Sun/)?.[0] ?? '', time.match(/\d/)?.[0] ?? ''])
-      .map(([day, period]) => day + period)
-  );
-
-  let scheduleSpanJa = null;
-  let scheduleSpanEn = null;
-
   if (titleJa.includes('学期前半')) {
-    scheduleSpanJa = '前半';
-    scheduleSpanEn = 'First half';
+    span.ja = '前半';
+    span.en = 'First half';
   } else if (titleJa.includes('学期後半')) {
-    scheduleSpanJa = '後半';
-    scheduleSpanEn = 'Second half';
+    span.ja = '後半';
+    span.en = 'Second half';
   }
 
   const schedule = {
-    year: scheduleYear,
-    semester: {
-      ja: scheduleSemesterJa,
-      en: scheduleSemesterEn,
-    },
-    times: {
-      ja: scheduleTimesJa,
-      en: scheduleTimesEn,
-    },
-    span: {
-      ja: scheduleSpanJa,
-      en: scheduleSpanEn,
-    },
+    year,
+    semester,
+    times,
+    span,
   };
 
   return schedule;
@@ -89,101 +88,147 @@ const buildCourseObject = (id: string, coursePages: CoursePage) => {
   const dom = cheerio.load(bodyJa);
   const domEn = cheerio.load(bodyEn);
 
-  const titleNameJa = dom('h2 .title').text();
-  const titleNameEn = domEn('h2 .title').text();
-  const titlePostscriptJa = null;
-  const titlePostscriptEn = null;
+  // Extracting
+
+  const title = {
+    ja: dom('h2 .title').text().trim(),
+    en: domEn('h2 .title').text().trim(),
+  };
+
+  const department = {
+    ja: undefined, // TODO
+    en: undefined, // TODO
+  };
 
   const lecturers = getLecturers(dom, domEn);
 
   const schedule = getSchedule(dom, domEn);
 
-  const classroom = getDD('開講場所', dom);
+  const location = {
+    ja: getDD('開講場所', dom),
+    en: getDD('Location', dom),
+  };
 
   const credit = parseInt(getDD('単位', dom), 10);
 
-  const languageJa = getDD('言語', dom);
-  const languageEn = getDD('Language', domEn);
+  const englishSupport = undefined; // TODO
 
-  const categoryJa = getDD('分野', dom);
-  const categoryEn = getDD('Field (Undergraduate) ', domEn);
+  const url = undefined; // TODO
 
-  const summaryJa = getDD('講義概要', dom);
-  const summaryEn = getDD('Course Summary', domEn);
+  const tools = {
+    ja: getDD('学生が利用する予定機材/ソフト等', dom), // TODO
+    en: getDD('Equipments & Software', domEn), // TODO
+  };
 
-  const typesJa = null;
-  const typesEn = null;
+  const teachingMaterials = {
+    ja: getDD('参考文献', dom), // TODO
+    en: getDD('Materials & Reading List', domEn), // TODO
+  };
 
-  const registrationNumber = null;
-  const registrationPrerequisiteMandatory = null;
-  const registrationPrerequisiteRecommended = null;
-  const registrationRequirementJa = null;
-  const registrationRequirementEn = null;
-  const registrationSuggestionJa = null;
-  const registrationSuggestionEn = null;
+  const summary = {
+    ja: getDD('講義概要', dom),
+    en: getDD('Course Summary', domEn),
+  };
 
-  const related = null;
+  const objectives = undefined; // TODO
+  const grading = undefined; // TODO
+  const plan = undefined; // TODO
 
-  const curriculumCode = getDD('科目ソート', dom);
-  const yearClassId = id;
+  const registrationPrerequisiteMandatory = undefined; // TODO
+  const registrationPrerequisiteRecommended = undefined; // TODO
+  const registrationRequirement = {
+    ja: undefined, // TODO
+    en: undefined, // TODO
+  };
+  const registrationSuggestion = {
+    ja: undefined, // TODO
+    en: undefined, // TODO
+  };
+  const advice = {
+    ja: getDD('履修上の注意・留意事項', dom), // TODO
+    en: getDD('Advice', domEn), // TODO
+  };
+  const duplicateCourses = {
+    ja: getDD('', dom), // TODO
+    en: getDD('', domEn), // TODO
+  };
 
-  const syllabusURL = null;
+  const needScreening = undefined; // TODO
+  const screening = undefined; // TODO
+  const quota = undefined; // TODO
+  const assignment = undefined; // TODO
+
+  const related = undefined; // TODO
+
+  const syllabusURL = undefined; // TODO
+
+  const aspect = undefined; // TODO
+
+  const category = {
+    ja: getDD('分野', dom),
+    en: getDD('Field (Undergraduate) ', domEn),
+  };
+
+  const classFormat = undefined; // TODO
+
+  const types = {
+    ja: getDD('授業形態', dom),
+    en: getDD('Class Style', domEn),
+  };
+
+  const language = {
+    ja: getDD('授業で使う言語', dom),
+    en: getDD('Language', domEn),
+  };
+
+  const tagCurriculumCode = getDD('科目ソート', dom);
 
   const tagGiga = getDD('GIGAサティフィケート対象', dom) === '対象';
 
-  const course: Course = {
-    title: {
-      name: {
-        ja: titleNameJa,
-        en: titleNameEn,
-      },
-      postscript: {
-        ja: titlePostscriptJa,
-        en: titlePostscriptEn,
-      },
-    },
+  const course: CourseV2 = {
+    id,
+    title,
+    department,
     lecturers,
     schedule,
-    classroom,
+    location,
     credit,
-    language: {
-      ja: languageJa,
-      en: languageEn,
-    },
-    summary: {
-      ja: summaryJa,
-      en: summaryEn,
-    },
-    types: {
-      ja: typesJa,
-      en: typesEn,
-    },
+    englishSupport,
+    url,
+    tools,
+    teachingMaterials,
+    summary,
+    objectives,
+    grading,
+    plan,
     registration: {
-      number: registrationNumber,
       prerequisite: {
         mandatory: registrationPrerequisiteMandatory,
         recommended: registrationPrerequisiteRecommended,
       },
-      requirement: {
-        ja: registrationRequirementJa,
-        en: registrationRequirementEn,
-      },
-      suggestion: {
-        ja: registrationSuggestionJa,
-        en: registrationSuggestionEn,
-      },
+      requirement: registrationRequirement,
+      suggestion: registrationSuggestion,
+      advice,
+      duplicateCourses,
+    },
+    screening: {
+      needScreening,
+      screening,
+      quota,
+      assignment,
     },
     related,
-    yearClassId,
     syllabusURL,
     tag: {
-      curriculumCode,
-      category: {
-        ja: categoryJa,
-        en: categoryEn,
-      },
+      aspect,
+      category,
+      classFormat,
+      types,
+      language,
+      curriculumCode: tagCurriculumCode,
       giga: tagGiga,
     },
+    version: 2,
   };
 
   return course;
